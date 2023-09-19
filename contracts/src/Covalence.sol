720efe97d4bee9e4b5fdb91c2e8da578f5d666eb
Orpheus Lummis <o@orpheuslummis.info>
2023-09-19 21:19:56 +0300

    WIP

--- contracts/src/Covalence.sol
+++ contracts/src/Covalence.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -17,17 +17,22 @@ contract Covalence is Ownable {
     struct Votes {
         address[] members;
         uint256[][] scores;
+        bool hasVoted;
     }
 
     struct Round {
         bool done;
     }
 
+    enum Methodology {linearCombination}
+
     Member[] public members;
     mapping(address => Votes) private votes;
     EvalDimensions private evalDimensions;
     mapping(address => uint256) public resultingShares;
     Round public round;
+    mapping(address => uint256) memberIndex;
+    Methodology public methodology;
 
     event RoundReadyToEnd();
     event NewRoundStarted();
@@ -39,7 +44,11 @@ contract Covalence is Ownable {
     }
 
     function setMembers(Member[] memory _members) public onlyOwner {
-        members = _members;
+        delete members;
+        for (uint256 i = 0; i < _members.length; i++) {
+            members.push(_members[i]);
+            memberIndex[_members[i].wallet] = i;
+        }
         emit NewMembersSet();
     }
 
@@ -49,9 +58,19 @@ contract Covalence is Ownable {
 
     function setDimensions(string[] memory dimensionNames, uint256[] memory weights) public onlyOwner {
         require(dimensionNames.length == weights.length, "Dimension names and weights length mismatch");
-        evalDimensions.name = dimensionNames;
-        evalDimensions.weight = weights;
+
+        delete evalDimensions.name;
+        delete evalDimensions.weight;
+
+        for (uint256 i = 0; i < dimensionNames.length; i++) {
+            evalDimensions.name.push(dimensionNames[i]);
+            evalDimensions.weight.push(weights[i]);
+        }
         emit NewDimensionsSet();
+    }
+
+    function getEvalDimensions() public view returns (string[] memory, uint256[] memory) {
+        return (evalDimensions.name, evalDimensions.weight);
     }
 
     function isRoundDone() public view returns (bool) {
@@ -59,9 +78,9 @@ contract Covalence is Ownable {
     }
 
     function startRound() public onlyOwner {
+        require(evalDimensions.name.length > 0, "Dimensions must be set before starting a round");
         for (uint256 i = 0; i < members.length; i++) {
-            delete votes[members[i].wallet].members;
-            delete votes[members[i].wallet].scores;
+            delete votes[members[i].wallet];
             resultingShares[members[i].wallet] = 0;
         }
         round.done = false;
@@ -70,20 +89,6 @@ contract Covalence is Ownable {
 
     function endRound() public onlyOwner {
         round.done = true;
-        emit RoundReadyToEnd();
-    }
-
-    function vote(uint256[][] memory scores) public {
-        require(isMember(msg.sender), "Only members can vote");
-        require(!round.done, "Round is not open");
-        require(votes[msg.sender].members.length == 0, "Member has already voted");
-
-        for (uint256 i = 0; i < members.length; i++) {
-            if (members[i].wallet != msg.sender) {
-                votes[msg.sender].members.push(members[i].wallet);
-                votes[msg.sender].scores.push(scores[i]);
-            }
-        }
         emit RoundReadyToEnd();
     }
 
@@ -96,8 +101,39 @@ contract Covalence is Ownable {
         return false;
     }
 
-    function getEvalDimensions() public view returns (string[] memory, uint256[] memory) {
-        return (evalDimensions.name, evalDimensions.weight);
+    /**
+     * Allows a member to vote in the current round.
+     * The member submits their scores for each other member in the form of a 2D array.
+     * The length of the outer array should be one less than the total number of members (excluding the voter).
+     * The length of each inner array should match the number of evaluation dimensions.
+     * Each score in the inner array corresponds to the voter's score for a particular member on a particular dimension.
+     * After voting, the member's vote status is updated to prevent them from voting again in the same round.
+     * Emits a RoundReadyToEnd event after a successful vote.
+     */
+    function vote(uint256[][] memory scores) public {
+        require(isMember(msg.sender), "Only members can vote");
+        require(!round.done, "Round is not open");
+        require(votes[msg.sender].members.length == 0, "Member has already voted");
+        require(!votes[msg.sender].hasVoted, "Member has already voted");
+        require(scores.length == (members.length - 1), "Scores array should be the number of other members.");
+
+        for (uint256 i = 0; i < scores.length; i++) {
+            require(
+                scores[i].length == evalDimensions.name.length,
+                "Inner scores array and evalDimensions must have the same length."
+            );
+        }
+
+        uint256 scoreIndex = 0;
+        for (uint256 i = 0; i < members.length; i++) {
+            if (members[i].wallet != msg.sender) {
+                votes[msg.sender].members.push(members[i].wallet);
+                votes[msg.sender].scores.push(scores[scoreIndex]);
+                scoreIndex++;
+            }
+        }
+        votes[msg.sender].hasVoted = true;
+        emit RoundReadyToEnd();
     }
 
     function getVotesForMember(address _member) public view returns (address[] memory, uint256[][] memory) {
@@ -105,17 +141,31 @@ contract Covalence is Ownable {
     }
 
     function computeRound() public {
+        require(members.length > 0, "No members to compute");
         require(round.done, "Round is not yet done");
-        methodologyLinearCombination();
+        if (methodology == Methodology.linearCombination) {
+            methodologyLinearCombination();
+        } else {
+            // TODO
+        }
     }
 
+    // compute shares of each members
+    // shares total 100%
+    // it is considering the scores of each members along the weighted dimensions
     function methodologyLinearCombination() private {
         uint256 totalScore = 0;
         mapping(address => uint256) storage memberScores = resultingShares;
 
+        // Resetting individual member scores
         for (uint256 i = 0; i < members.length; i++) {
             address memberWallet = members[i].wallet;
             memberScores[memberWallet] = 0;
+        }
+
+        // Calculating individual member scores
+        for (uint256 i = 0; i < members.length; i++) {
+            address memberWallet = members[i].wallet;
 
             for (uint256 k = 0; k < evalDimensions.weight.length; k++) {
                 uint256 dimensionWeight = evalDimensions.weight[k];
@@ -125,14 +175,25 @@ contract Covalence is Ownable {
                     memberScores[memberWallet] += scoreForDimension * dimensionWeight;
                 }
             }
+
             totalScore += memberScores[memberWallet];
         }
 
         require(totalScore > 0, "Total score must be greater than zero to avoid division by zero.");
 
-        for (uint256 i = 0; i < members.length; i++) {
+        // Normalizing individual member scores to make up 100%
+        uint256 remainingPercentage = 100;
+        address lastMemberWallet = members[members.length - 1].wallet;
+
+        for (uint256 i = 0; i < members.length - 1; i++) {
             address memberWallet = members[i].wallet;
-            resultingShares[memberWallet] = (memberScores[memberWallet] * 100) / totalScore;
+            uint256 normalizedShare = (memberScores[memberWallet] * 100) / totalScore;
+
+            resultingShares[memberWallet] = normalizedShare;
+            remainingPercentage -= normalizedShare;
         }
+
+        // To ensure total percentage equals 100%, allocate the remaining percentage to the last member.
+        resultingShares[lastMemberWallet] = remainingPercentage;
     }
 }
--- contracts/test/Covalence.t.sol
+++ contracts/test/Covalence.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {Covalence} from "../src/Covalence.sol";
@@ -39,6 +39,14 @@ contract CovalenceTest is Test {
     }
 
     function test_StartEndRound() public {
+        string[] memory names = new string[](2);
+        uint256[] memory weights = new uint256[](2);
+        names[0] = "Quality";
+        names[1] = "Speed";
+        weights[0] = 3;
+        weights[1] = 1;
+        covalence.setDimensions(names, weights);
+
         covalence.startRound();
         assertEq(covalence.isRoundDone(), false);
 
@@ -52,23 +60,28 @@ contract CovalenceTest is Test {
         _members[1] = Covalence.Member(address(0xDef), "Bob");
         covalence.setMembers(_members);
 
-        string[] memory names = new string[](1);
-        uint256[] memory weights = new uint256[](1);
+        string[] memory names = new string[](2);
+        uint256[] memory weights = new uint256[](2);
         names[0] = "Quality";
-        weights[0] = 1;
+        names[1] = "Speed";
+        weights[0] = 3;
+        weights[1] = 1;
         covalence.setDimensions(names, weights);
 
         covalence.startRound();
 
         uint256[][] memory scores = new uint256[][](1);
-        scores[0] = new uint256[](1);
+        scores[0] = new uint256[](2);
         scores[0][0] = 7;
-        covalence.vote(scores);
+        scores[0][1] = 5;
 
-        (address[] memory fetchedMembers, uint256[][] memory fetchedScores) =
-            covalence.getVotesForMember(address(0xAbc));
-        assertEq(fetchedMembers[0], address(0xDef));
-        assertEq(fetchedScores[0][0], 7);
+        bool success = true;
+        vm.prank(address(0xAbc));
+        try covalence.vote(scores) {}
+        catch {
+            success = false;
+        }
+        assertEq(success, true);
     }
 
     function test_ComputeRound() public {
@@ -84,20 +97,37 @@ contract CovalenceTest is Test {
         covalence.setDimensions(names, weights);
 
         covalence.startRound();
-        uint256[][] memory scores = new uint256[][](1);
-        scores[0] = new uint256[](1);
-        scores[0][0] = 7;
-        covalence.vote(scores);
+
+        uint256[][] memory scoresAlice = new uint256[][](1);
+        scoresAlice[0] = new uint256[](1);
+        scoresAlice[0][0] = 5;
+        vm.prank(address(0xAbc));
+        covalence.vote(scoresAlice);
+
+        uint256[][] memory scoresBob = new uint256[][](1);
+        scoresBob[0] = new uint256[](1);
+        scoresBob[0][0] = 5;
+        vm.prank(address(0xDef));
+        covalence.vote(scoresBob);
 
         covalence.endRound();
-        covalence.computeRound();
 
-        // TBD
-        assertEq(covalence.resultingShares(address(0xAbc)), 11);
-        assertEq(covalence.resultingShares(address(0xDef)), 89);
+        assertEq(covalence.resultingShares(address(0xAbc)), 47);
+        assertEq(covalence.resultingShares(address(0xDef)), 53);
     }
 
     function test_NonMemberCannotVote() public {
+        string[] memory names = new string[](1);
+        uint256[] memory weights = new uint256[](1);
+        names[0] = "Quality";
+        weights[0] = 1;
+        covalence.setDimensions(names, weights);
+
+        Covalence.Member[] memory _members = new Covalence.Member[](2);
+        _members[0] = Covalence.Member(address(0xAbc), "Alice");
+        _members[1] = Covalence.Member(address(0xDef), "Bob");
+        covalence.setMembers(_members);
+
         covalence.startRound();
 
         uint256[][] memory scores = new uint256[][](1);
