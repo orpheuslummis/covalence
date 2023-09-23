ad44e9fb6c7b4e775e9219cf5274bf5dd291befb
Orpheus Lummis <o@orpheuslummis.info>
2023-09-23 23:02:52 +0300

    group management update

--- contracts/src/GroupManagement.sol
+++ contracts/src/GroupManagement.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
 /*
-group management system
-there is one contract 'owner' with admin rights over the system
-users can create a new group
-the creator a group is its admin
-the admin can transfer its admin role to a different user
-the admin can add or remove members to the group
-users can leave gruops
+A group management system.
+- Contract owner (SYSTEM_ADMIN_ROLE) has admin rights over the system.
+- Users can create a new group.
+- The creator of a group is its admin.
+- The admin can transfer its admin role to a different user.
+- The admin can add or remove members to/from the group.
+- Users can leave groups.
+- Admin cannot leave their own group.
 */
 
 contract GroupManagement is AccessControl {
@@ -30,13 +31,13 @@ contract GroupManagement is AccessControl {
     event UserJoinedGroup(uint256 groupId, address indexed user);
     event UserLeftGroup(uint256 groupId, address indexed user);
 
-    modifier onlySystemAdmin() {
-        _checkRole(SYSTEM_ADMIN_ROLE, msg.sender);
+    modifier groupExists(uint256 groupId) {
+        require(groups[groupId].cid != bytes32(0), "Group does not exist");
         _;
     }
 
-    modifier groupExists(uint256 groupId) {
-        require(groups[groupId].cid != bytes32(0), "Group does not exist");
+    modifier onlyGroupAdmin(uint256 groupId) {
+        require(hasRole(_getGroupAdminRole(groupId), msg.sender), "Unauthorized");
         _;
     }
 
@@ -44,43 +45,79 @@ contract GroupManagement is AccessControl {
         _setupRole(SYSTEM_ADMIN_ROLE, msg.sender);
     }
 
-    function createGroup(bytes32 cid) external {
+    function _getGroupAdminRole(uint256 groupId) internal pure returns (bytes32) {
+        return keccak256(abi.encodePacked("GROUP_ADMIN_ROLE", groupId));
+    }
+
+    function createGroup(bytes32 cid, address[] calldata initialMembers) external {
         require(cid != bytes32(0), "CID cannot be empty");
+
         groups[nextGroupId] = Group(cid);
-        bytes32 groupAdminRole = keccak256(abi.encodePacked("GROUP_ADMIN_ROLE", nextGroupId));
-        _setupRole(groupAdminRole, msg.sender);
+        _setupRole(_getGroupAdminRole(nextGroupId), msg.sender);
+
+        groupMembers[nextGroupId][msg.sender] = true;
+
+        for (uint256 i = 0; i < initialMembers.length; i++) {
+            groupMembers[nextGroupId][initialMembers[i]] = true;
+        }
+
         emit GroupCreated(nextGroupId, cid, msg.sender);
         nextGroupId++;
     }
 
-    function changeAdmin(uint256 groupId, address newAdmin) external groupExists(groupId) {
-        bytes32 groupAdminRole = keccak256(abi.encodePacked("GROUP_ADMIN_ROLE", groupId));
-        require(hasRole(groupAdminRole, msg.sender), "Unauthorized");
-        _setupRole(groupAdminRole, newAdmin);
-        _revokeRole(groupAdminRole, msg.sender);
+    function changeAdmin(uint256 groupId, address newAdmin) external groupExists(groupId) onlyGroupAdmin(groupId) {
+        _setupRole(_getGroupAdminRole(groupId), newAdmin);
+        _revokeRole(_getGroupAdminRole(groupId), msg.sender);
+
         emit AdminChanged(groupId, newAdmin, msg.sender);
     }
 
-    function addMember(uint256 groupId, address newMember) external groupExists(groupId) {
-        bytes32 groupAdminRole = keccak256(abi.encodePacked("GROUP_ADMIN_ROLE", groupId));
-        require(hasRole(groupAdminRole, msg.sender), "Unauthorized");
+    function addMember(uint256 groupId, address newMember) external groupExists(groupId) onlyGroupAdmin(groupId) {
         groupMembers[groupId][newMember] = true;
         emit UserJoinedGroup(groupId, newMember);
     }
 
-    function removeMember(uint256 groupId, address member) external groupExists(groupId) {
-        bytes32 groupAdminRole = keccak256(abi.encodePacked("GROUP_ADMIN_ROLE", groupId));
-        require(hasRole(groupAdminRole, msg.sender), "Unauthorized");
+    function removeMember(uint256 groupId, address member) external groupExists(groupId) onlyGroupAdmin(groupId) {
+        require(groupMembers[groupId][member], "Member does not exist");
         delete groupMembers[groupId][member];
         emit UserLeftGroup(groupId, member);
     }
 
     function leaveGroup(uint256 groupId) external groupExists(groupId) {
+        require(!hasRole(_getGroupAdminRole(groupId), msg.sender), "Group admin cannot leave their own group");
+
         delete groupMembers[groupId][msg.sender];
         emit UserLeftGroup(groupId, msg.sender);
     }
 
     function isMemberOfGroup(uint256 groupId, address user) external view groupExists(groupId) returns (bool) {
         return groupMembers[groupId][user];
+    }
+
+    function addMembers(uint256 groupId, address[] calldata newMembers)
+        external
+        groupExists(groupId)
+        onlyGroupAdmin(groupId)
+    {
+        for (uint256 i = 0; i < newMembers.length; i++) {
+            groupMembers[groupId][newMembers[i]] = true;
+            emit UserJoinedGroup(groupId, newMembers[i]);
+        }
+    }
+
+    function removeMembers(uint256 groupId, address[] calldata members)
+        external
+        groupExists(groupId)
+        onlyGroupAdmin(groupId)
+    {
+        for (uint256 i = 0; i < members.length; i++) {
+            require(groupMembers[groupId][members[i]], "Member does not exist");
+            delete groupMembers[groupId][members[i]];
+            emit UserLeftGroup(groupId, members[i]);
+        }
+    }
+
+    function updateGroupCID(uint256 groupId, bytes32 newCID) external groupExists(groupId) onlyGroupAdmin(groupId) {
+        groups[groupId].cid = newCID;
     }
 }
