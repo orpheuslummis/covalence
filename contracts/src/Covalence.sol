ede50ff1032596e3bce125ce860217887af038b9
Orpheus Lummis <o@orpheuslummis.info>
2023-09-24 18:04:55 +0300

    WIP

--- contracts/src/Covalence.sol
+++ contracts/src/Covalence.sol
@@ -1,199 +1,274 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+/*
+Covalence is a decentralized platform for managing group evaluations and value distribution.
+This contract enables groups to form, each capable of conducting independent evaluation rounds based on flexible evaluation methodologies.
 
-contract Covalence is Ownable {
-    struct Member {
-        address wallet;
+Group Management: Each group is controlled by an admin and consists of multiple members. The admin manages the evaluation rounds within the group.
+
+Evaluation Methodologies: Groups can employ various evaluation methodologies, currently including "LinearCombination" and other methodologies to be decided.
+
+Evaluation Dimensions: The specific evaluation criteria, termed 'evalDimensions,' are flexible and customizable per each group's chosen methodology.
+
+Round Management: Groups can initiate, conduct, and complete evaluation rounds, which take member evaluations and compute a distribution of values.
+
+Future Features:
+- Introduce a composable language for custom evaluation methodologies.
+- Provide template methodologies for common evaluation scenarios.
+*/
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./GroupManagement.sol";
+
+contract Covalence is Ownable, GroupManagement {
+    struct MemberData {
         string name;
     }
 
     struct EvalDimensions {
-        string[] name;
-        uint256[] weight;
+        string[] names;
+        uint256[] weights;
     }
 
-    struct Votes {
-        address[] members;
-        uint256[][] scores;
-        bool hasVoted;
+    enum EvalMethodology {
+        LinearCombination,
+        TBD
+    }
+
+    enum RoundStatus {
+        Closed,
+        Open,
+        Completed
     }
 
     struct Round {
-        bool done;
+        mapping(address => uint256[][]) scores; // scores indexed by member address
+        mapping(address => bool) memberHasEvaluated;
+        mapping(address => uint256) resultDistribution;
+        RoundStatus status;
     }
 
-    enum Methodology {linearCombination}
-
-    Member[] public members;
-    mapping(address => Votes) private votes;
-    EvalDimensions private evalDimensions;
-    mapping(address => uint256) public resultingShares;
-    Round public round;
-    mapping(address => uint256) memberIndex;
-    Methodology public methodology;
-
-    event RoundReadyToEnd();
-    event NewRoundStarted();
-    event NewMembersSet();
-    event NewDimensionsSet();
-
-    constructor() {
-        round.done = true;
+    struct CovalenceGroup {
+        uint256 groupId;
+        EvalMethodology methodology;
+        EvalDimensions evalDimensions;
+        Round[] rounds;
     }
 
-    function setMembers(Member[] memory _members) public onlyOwner {
-        delete members;
-        for (uint256 i = 0; i < _members.length; i++) {
-            members.push(_members[i]);
-            memberIndex[_members[i].wallet] = i;
-        }
-        emit NewMembersSet();
+    mapping(uint256 => CovalenceGroup) covalenceGroups;
+
+    event EvalDimensionsUpdated(uint256 groupId);
+    event EvalMethodologyUpdated(uint256 groupId);
+    event RoundOpen(uint256 groupId, uint256 roundNumber);
+    event RoundSet(uint256 groupId, uint256 roundNumber);
+    event RoundDimensionsSet(uint256 groupId, uint256 roundNumber);
+    event RoundStarted(uint256 groupId, uint256 roundNumber, uint256 memberCount, uint256 dimensionCount);
+    event RoundReadyToEnd(uint256 groupId, uint256 roundNumber);
+    event RoundEnded(uint256 groupId, uint256 roundNumber, RoundStatus status);
+
+    modifier isRoundOpen(uint256 groupId) {
+        require(
+            covalenceGroups[groupId].rounds.length > 0
+                && covalenceGroups[groupId].rounds[covalenceGroups[groupId].rounds.length - 1].status == RoundStatus.Open,
+            "No round is open"
+        );
+        _;
     }
 
-    function getMembers() public view returns (Member[] memory) {
-        return members;
+    modifier isRoundClosed(uint256 groupId) {
+        require(
+            covalenceGroups[groupId].rounds.length == 0
+                || covalenceGroups[groupId].rounds[covalenceGroups[groupId].rounds.length - 1].status != RoundStatus.Open,
+            "A round is already open"
+        );
+        _;
     }
 
-    function setDimensions(string[] memory dimensionNames, uint256[] memory weights) public onlyOwner {
+    function setMethodology(uint256 groupId, EvalMethodology newMethodology) external onlyGroupAdmin(groupId) {
+        covalenceGroups[groupId].methodology = newMethodology;
+        emit EvalMethodologyUpdated(groupId);
+    }
+
+    function setDimensions(uint256 groupId, string[] memory dimensionNames, uint256[] memory weights)
+        public
+        onlyGroupAdmin(groupId)
+    {
         require(dimensionNames.length == weights.length, "Dimension names and weights length mismatch");
-
-        delete evalDimensions.name;
-        delete evalDimensions.weight;
-
-        for (uint256 i = 0; i < dimensionNames.length; i++) {
-            evalDimensions.name.push(dimensionNames[i]);
-            evalDimensions.weight.push(weights[i]);
-        }
-        emit NewDimensionsSet();
+        covalenceGroups[groupId].evalDimensions.names = dimensionNames;
+        covalenceGroups[groupId].evalDimensions.weights = weights;
+        emit EvalDimensionsUpdated(groupId);
     }
 
-    function getEvalDimensions() public view returns (string[] memory, uint256[] memory) {
-        return (evalDimensions.name, evalDimensions.weight);
+    function getRoundStatus(uint256 groupId, uint256 roundNumber) public view returns (RoundStatus) {
+        return covalenceGroups[groupId].rounds[roundNumber].status;
     }
 
-    function isRoundDone() public view returns (bool) {
-        return round.done;
+    function startRound(uint256 groupId) external onlyGroupAdmin(groupId) isRoundClosed(groupId) {
+        require(getGroupMemberCount(groupId) > 0, "No members to participate");
+        require(covalenceGroups[groupId].evalDimensions.names.length > 0, "Evaluation dimensions not set");
+
+        covalenceGroups[groupId].rounds.push();
+        Round storage newRound = covalenceGroups[groupId].rounds[covalenceGroups[groupId].rounds.length - 1];
+        newRound.status = RoundStatus.Open;
+
+        emit RoundStarted(
+            groupId,
+            covalenceGroups[groupId].rounds.length - 1,
+            getGroupMemberCount(groupId),
+            covalenceGroups[groupId].evalDimensions.names.length
+        );
     }
 
-    function startRound() public onlyOwner {
-        require(evalDimensions.name.length > 0, "Dimensions must be set before starting a round");
-        for (uint256 i = 0; i < members.length; i++) {
-            delete votes[members[i].wallet];
-            resultingShares[members[i].wallet] = 0;
-        }
-        round.done = false;
-        emit NewRoundStarted();
-    }
-
-    function endRound() public onlyOwner {
-        round.done = true;
-        emit RoundReadyToEnd();
-    }
-
-    function isMember(address member) private view returns (bool) {
-        for (uint256 i = 0; i < members.length; i++) {
-            if (members[i].wallet == member) {
-                return true;
+    function allMembersEvaluated(uint256 groupId, uint256 roundNumber) private view returns (bool) {
+        address[] memory groupMembers = getGroupMembers(groupId);
+        for (uint256 i = 0; i < groupMembers.length; i++) {
+            if (!covalenceGroups[groupId].rounds[roundNumber].memberHasEvaluated[groupMembers[i]]) {
+                return false;
             }
         }
-        return false;
+        return true;
+    }
+
+    function endRound(uint256 groupId) external onlyGroupAdmin(groupId) isRoundOpen(groupId) {
+        uint256 roundNumber = covalenceGroups[groupId].rounds.length - 1;
+        require(allMembersEvaluated(groupId, roundNumber), "Not all members have evaluated.");
+
+        Round storage currentRound = covalenceGroups[groupId].rounds[roundNumber];
+
+        if (covalenceGroups[groupId].methodology == EvalMethodology.LinearCombination) {
+            methodologyLinearCombination(groupId, roundNumber);
+        } else {
+            revert("Invalid methodology");
+        }
+
+        currentRound.status = RoundStatus.Completed;
+
+        emit RoundEnded(groupId, covalenceGroups[groupId].rounds.length - 1, currentRound.status);
     }
 
     /**
-     * Allows a member to vote in the current round.
-     * The member submits their scores for each other member in the form of a 2D array.
-     * The length of the outer array should be one less than the total number of members (excluding the voter).
+     * The member submits scores for each member, including themselves, in the form of a 2D array.
+     * The length of the outer array should match the total number of members.
      * The length of each inner array should match the number of evaluation dimensions.
-     * Each score in the inner array corresponds to the voter's score for a particular member on a particular dimension.
-     * After voting, the member's vote status is updated to prevent them from voting again in the same round.
-     * Emits a RoundReadyToEnd event after a successful vote.
+     * Each score in the inner array corresponds to the evaluator's score for a particular member on a particular dimension.
+     * After voting, the member's evaluation status is updated to prevent them from scoring again in the same round.
+     * Emits a RoundReadyToEnd event after a successful evaluation if all members have evaluated.
      */
-    function vote(uint256[][] memory scores) public {
-        require(isMember(msg.sender), "Only members can vote");
-        require(!round.done, "Round is not open");
-        require(votes[msg.sender].members.length == 0, "Member has already voted");
-        require(!votes[msg.sender].hasVoted, "Member has already voted");
-        require(scores.length == (members.length - 1), "Scores array should be the number of other members.");
+    function eval(uint256 groupId, uint256 roundNumber, uint256[][] memory scores) public {
+        require(isMemberOfGroup(groupId, msg.sender), "Only members can evaluate");
+        require(covalenceGroups[groupId].rounds[roundNumber].status == RoundStatus.Open, "Round is not open");
+        require(
+            !covalenceGroups[groupId].rounds[roundNumber].memberHasEvaluated[msg.sender], "Member has already evaluated"
+        );
+        require(scores.length == getGroupMemberCount(groupId), "Scores array should match the number of members.");
 
+        // Ensure that the length of each inner array matches the number of evaluation dimensions.
         for (uint256 i = 0; i < scores.length; i++) {
             require(
-                scores[i].length == evalDimensions.name.length,
+                scores[i].length == covalenceGroups[groupId].evalDimensions.names.length,
                 "Inner scores array and evalDimensions must have the same length."
             );
         }
 
-        uint256 scoreIndex = 0;
-        for (uint256 i = 0; i < members.length; i++) {
-            if (members[i].wallet != msg.sender) {
-                votes[msg.sender].members.push(members[i].wallet);
-                votes[msg.sender].scores.push(scores[scoreIndex]);
-                scoreIndex++;
-            }
+        for (uint256 i = 0; i < getGroupMemberCount(groupId); i++) {
+            covalenceGroups[groupId].rounds[roundNumber].scores[msg.sender][i] = scores[i];
         }
-        votes[msg.sender].hasVoted = true;
-        emit RoundReadyToEnd();
-    }
 
-    function getVotesForMember(address _member) public view returns (address[] memory, uint256[][] memory) {
-        return (votes[_member].members, votes[_member].scores);
-    }
+        covalenceGroups[groupId].rounds[roundNumber].memberHasEvaluated[msg.sender] = true;
 
-    function computeRound() public {
-        require(members.length > 0, "No members to compute");
-        require(round.done, "Round is not yet done");
-        if (methodology == Methodology.linearCombination) {
-            methodologyLinearCombination();
-        } else {
-            // TODO
+        if (allMembersEvaluated(groupId, roundNumber)) {
+            emit RoundReadyToEnd(groupId, roundNumber);
         }
     }
 
-    // compute shares of each members
-    // shares total 100%
-    // it is considering the scores of each members along the weighted dimensions
-    function methodologyLinearCombination() private {
+    function getEvalsOfMember(uint256 groupId, uint256 roundNumber, address _member)
+        public
+        view
+        returns (uint256[][] memory)
+    {
+        return covalenceGroups[groupId].rounds[roundNumber].scores[_member];
+    }
+
+    function getEvalDimensions(uint256 groupId) public view returns (string[] memory, uint256[] memory) {
+        return (covalenceGroups[groupId].evalDimensions.names, covalenceGroups[groupId].evalDimensions.weights);
+    }
+
+    function methodologyLinearCombination(uint256 groupId, uint256 roundNumber) private {
         uint256 totalScore = 0;
-        mapping(address => uint256) storage memberScores = resultingShares;
+        uint256[] memory individualScores = new uint256[](getGroupMemberCount(groupId));
 
-        // Resetting individual member scores
-        for (uint256 i = 0; i < members.length; i++) {
-            address memberWallet = members[i].wallet;
-            memberScores[memberWallet] = 0;
+        uint256 evalDimensionWeightsSum = 0;
+        for (uint256 i = 0; i < covalenceGroups[groupId].evalDimensions.weights.length; i++) {
+            evalDimensionWeightsSum += covalenceGroups[groupId].evalDimensions.weights[i];
         }
+        require(evalDimensionWeightsSum > 0, "Sum of dimension weights must be greater than zero.");
 
-        // Calculating individual member scores
-        for (uint256 i = 0; i < members.length; i++) {
-            address memberWallet = members[i].wallet;
+        // Accumulate individual scores
+        for (uint256 i = 0; i < getGroupMemberCount(groupId); i++) {
+            address memberWallet = getGroupMembers(groupId)[i];
+            uint256 individualScore = 0;
 
-            for (uint256 k = 0; k < evalDimensions.weight.length; k++) {
-                uint256 dimensionWeight = evalDimensions.weight[k];
-
-                for (uint256 j = 0; j < votes[memberWallet].members.length; j++) {
-                    uint256 scoreForDimension = votes[memberWallet].scores[j][k];
-                    memberScores[memberWallet] += scoreForDimension * dimensionWeight;
+            for (uint256 j = 0; j < getGroupMemberCount(groupId); j++) {
+                for (uint256 k = 0; k < covalenceGroups[groupId].evalDimensions.weights.length; k++) {
+                    uint256 dimensionWeight = covalenceGroups[groupId].evalDimensions.weights[k];
+                    uint256 memberScore = covalenceGroups[groupId].rounds[roundNumber].scores[memberWallet][j][k];
+                    individualScore += (memberScore * dimensionWeight) / evalDimensionWeightsSum;
                 }
             }
 
-            totalScore += memberScores[memberWallet];
+            individualScores[i] = individualScore;
+            totalScore += individualScore;
         }
 
-        require(totalScore > 0, "Total score must be greater than zero to avoid division by zero.");
+        // Safety check for division by zero
+        require(totalScore > 0, "Total score must be greater than zero.");
 
-        // Normalizing individual member scores to make up 100%
-        uint256 remainingPercentage = 100;
-        address lastMemberWallet = members[members.length - 1].wallet;
+        // Compute shares
+        for (uint256 i = 0; i < getGroupMemberCount(groupId); i++) {
+            address memberWallet = getGroupMembers(groupId)[i];
+            uint256 share = (individualScores[i] * 1e18) / totalScore; // Fixed-point arithmetic for more precision
+            covalenceGroups[groupId].rounds[roundNumber].resultDistribution[memberWallet] = share;
+        }
+    }
 
-        for (uint256 i = 0; i < members.length - 1; i++) {
-            address memberWallet = members[i].wallet;
-            uint256 normalizedShare = (memberScores[memberWallet] * 100) / totalScore;
+    struct AddressToUintPair {
+        address key;
+        uint256 value;
+    }
 
-            resultingShares[memberWallet] = normalizedShare;
-            remainingPercentage -= normalizedShare;
+    function getRoundResult(uint256 groupId, uint256 roundNumber) external view returns (AddressToUintPair[] memory) {
+        require(covalenceGroups[groupId].rounds[roundNumber].status == RoundStatus.Completed, "Round not yet ended");
+
+        address[] memory groupMembers = getGroupMembers(groupId);
+        AddressToUintPair[] memory results = new AddressToUintPair[](groupMembers.length);
+
+        for (uint256 i = 0; i < groupMembers.length; i++) {
+            results[i] = AddressToUintPair(
+                groupMembers[i], covalenceGroups[groupId].rounds[roundNumber].resultDistribution[groupMembers[i]]
+            );
         }
 
-        // To ensure total percentage equals 100%, allocate the remaining percentage to the last member.
-        resultingShares[lastMemberWallet] = remainingPercentage;
+        return results;
+    }
+
+    function getMemberEvaluation(uint256 groupId, uint256 roundNumber, address member)
+        external
+        view
+        returns (uint256[] memory)
+    {
+        require(covalenceGroups[groupId].rounds[roundNumber].memberHasEvaluated[member], "Member did not evaluate");
+        uint256 index = findMemberIndex(groupId, member);
+        return covalenceGroups[groupId].rounds[roundNumber].scores[member][index];
+    }
+
+    function findMemberIndex(uint256 groupId, address member) private view returns (uint256) {
+        address[] memory groupMembers = getGroupMembers(groupId);
+        for (uint256 i = 0; i < groupMembers.length; i++) {
+            if (groupMembers[i] == member) {
+                return i;
+            }
+        }
+        revert("Member not found");
     }
 }
--- contracts/src/GroupManagement.sol
+++ contracts/src/GroupManagement.sol
@@ -16,20 +16,26 @@ A group management system.
 
 contract GroupManagement is AccessControl {
     bytes32 public constant SYSTEM_ADMIN_ROLE = keccak256("SYSTEM_ADMIN_ROLE");
+    uint256 public constant batchLimit = 50;
 
     struct Group {
+        string name;
         bytes32 cid; // Content Identifier for off-chain metadata
+        address[] members;
     }
 
     mapping(uint256 => Group) public groups;
-    mapping(uint256 => mapping(address => bool)) public groupMembers;
 
-    uint256 public nextGroupId = 1;
+    uint256 public nextGroupId;
 
     event GroupCreated(uint256 groupId, bytes32 cid, address indexed admin);
     event AdminChanged(uint256 groupId, address indexed newAdmin, address indexed oldAdmin);
     event UserJoinedGroup(uint256 groupId, address indexed user);
     event UserLeftGroup(uint256 groupId, address indexed user);
+    event CIDUpdated(uint256 groupId, bytes32 newCID, bytes32 oldCID);
+    event SystemGroupDestroyed(uint256 groupId, address indexed admin);
+    event SystemUserRemoved(uint256 groupId, address indexed user);
+    event GroupNameUpdated(uint256 groupId, string newName, string oldName);
 
     modifier groupExists(uint256 groupId) {
         require(groups[groupId].cid != bytes32(0), "Group does not exist");
@@ -45,53 +51,75 @@ contract GroupManagement is AccessControl {
         _setupRole(SYSTEM_ADMIN_ROLE, msg.sender);
     }
 
+    function getGroupAdminRole(uint256 groupId) public pure returns (bytes32) {
+        return _getGroupAdminRole(groupId);
+    }
+
     function _getGroupAdminRole(uint256 groupId) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked("GROUP_ADMIN_ROLE", groupId));
     }
 
-    function createGroup(bytes32 cid, address[] calldata initialMembers) external {
+    function createGroup(string memory name, bytes32 cid, address[] calldata initialMembers) external {
+        require(initialMembers.length <= 100, "Too many initial members");
         require(cid != bytes32(0), "CID cannot be empty");
 
-        groups[nextGroupId] = Group(cid);
+        for (uint256 i = 0; i < nextGroupId; i++) {
+            require(groups[i].cid != cid, "Group with this CID already exists");
+            require(
+                keccak256(abi.encodePacked(groups[i].name)) != keccak256(abi.encodePacked(name)),
+                "Group with this name already exists"
+            );
+        }
+
+        groups[nextGroupId].name = name;
+        groups[nextGroupId].cid = cid;
         _setupRole(_getGroupAdminRole(nextGroupId), msg.sender);
 
-        groupMembers[nextGroupId][msg.sender] = true;
+        groups[nextGroupId].members.push(msg.sender);
 
         for (uint256 i = 0; i < initialMembers.length; i++) {
-            groupMembers[nextGroupId][initialMembers[i]] = true;
+            require(initialMembers[i] != address(0), "Member address cannot be zero");
+            require(!isMemberOfGroup(nextGroupId, initialMembers[i]), "Duplicate member");
+            groups[nextGroupId].members.push(initialMembers[i]);
         }
 
         emit GroupCreated(nextGroupId, cid, msg.sender);
         nextGroupId++;
     }
 
+    // Add a safeguard against orphaning a group
     function changeAdmin(uint256 groupId, address newAdmin) external groupExists(groupId) onlyGroupAdmin(groupId) {
+        require(newAdmin != address(0), "New admin cannot be the zero address");
         _setupRole(_getGroupAdminRole(groupId), newAdmin);
         _revokeRole(_getGroupAdminRole(groupId), msg.sender);
-
         emit AdminChanged(groupId, newAdmin, msg.sender);
     }
 
-    function addMember(uint256 groupId, address newMember) external groupExists(groupId) onlyGroupAdmin(groupId) {
-        groupMembers[groupId][newMember] = true;
+    function addMember(uint256 groupId, address newMember) public groupExists(groupId) onlyGroupAdmin(groupId) {
+        require(newMember != address(0), "Member address cannot be zero");
+        require(!isMemberOfGroup(groupId, newMember), "Member already exists");
+        groups[groupId].members.push(newMember);
         emit UserJoinedGroup(groupId, newMember);
     }
 
     function removeMember(uint256 groupId, address member) external groupExists(groupId) onlyGroupAdmin(groupId) {
-        require(groupMembers[groupId][member], "Member does not exist");
-        delete groupMembers[groupId][member];
+        _removeMember(groupId, member);
         emit UserLeftGroup(groupId, member);
     }
 
     function leaveGroup(uint256 groupId) external groupExists(groupId) {
         require(!hasRole(_getGroupAdminRole(groupId), msg.sender), "Group admin cannot leave their own group");
-
-        delete groupMembers[groupId][msg.sender];
+        _removeMember(groupId, msg.sender);
         emit UserLeftGroup(groupId, msg.sender);
     }
 
-    function isMemberOfGroup(uint256 groupId, address user) external view groupExists(groupId) returns (bool) {
-        return groupMembers[groupId][user];
+    function isMemberOfGroup(uint256 groupId, address user) public view groupExists(groupId) returns (bool) {
+        for (uint256 i = 0; i < groups[groupId].members.length; i++) {
+            if (groups[groupId].members[i] == user) {
+                return true;
+            }
+        }
+        return false;
     }
 
     function addMembers(uint256 groupId, address[] calldata newMembers)
@@ -99,9 +127,9 @@ contract GroupManagement is AccessControl {
         groupExists(groupId)
         onlyGroupAdmin(groupId)
     {
+        require(newMembers.length <= batchLimit, "Too many members to add at once");
         for (uint256 i = 0; i < newMembers.length; i++) {
-            groupMembers[groupId][newMembers[i]] = true;
-            emit UserJoinedGroup(groupId, newMembers[i]);
+            addMember(groupId, newMembers[i]);
         }
     }
 
@@ -111,13 +139,66 @@ contract GroupManagement is AccessControl {
         onlyGroupAdmin(groupId)
     {
         for (uint256 i = 0; i < members.length; i++) {
-            require(groupMembers[groupId][members[i]], "Member does not exist");
-            delete groupMembers[groupId][members[i]];
-            emit UserLeftGroup(groupId, members[i]);
+            _removeMember(groupId, members[i]);
         }
     }
 
     function updateGroupCID(uint256 groupId, bytes32 newCID) external groupExists(groupId) onlyGroupAdmin(groupId) {
+        bytes32 oldCID = groups[groupId].cid;
         groups[groupId].cid = newCID;
+        emit CIDUpdated(groupId, newCID, oldCID);
+    }
+
+    function updateGroupName(uint256 groupId, string memory newName)
+        external
+        groupExists(groupId)
+        onlyGroupAdmin(groupId)
+    {
+        string memory oldName = groups[groupId].name;
+        groups[groupId].name = newName;
+        emit GroupNameUpdated(groupId, newName, oldName);
+    }
+
+    function getGroupInfo(uint256 groupId) external view returns (string memory, bytes32) {
+        return (groups[groupId].name, groups[groupId].cid);
+    }
+
+    function systemRemoveMember(uint256 groupId, address member) external {
+        require(hasRole(SYSTEM_ADMIN_ROLE, msg.sender), "Unauthorized");
+        _removeMember(groupId, member);
+        emit SystemUserRemoved(groupId, member);
+    }
+
+    function systemReplaceGroupAdmin(uint256 groupId, address newAdmin) external {
+        require(hasRole(SYSTEM_ADMIN_ROLE, msg.sender), "Unauthorized");
+        require(newAdmin != address(0), "New admin cannot be the zero address");
+        _setupRole(_getGroupAdminRole(groupId), newAdmin);
+        emit AdminChanged(groupId, newAdmin, msg.sender);
+    }
+
+    function systemDestroyGroup(uint256 groupId) external {
+        require(hasRole(SYSTEM_ADMIN_ROLE, msg.sender), "Unauthorized");
+        require(groups[groupId].cid != bytes32(0), "Group does not exist");
+        delete groups[groupId];
+        emit SystemGroupDestroyed(groupId, msg.sender);
+    }
+
+    function getGroupMemberCount(uint256 groupId) public view returns (uint256) {
+        return groups[groupId].members.length;
+    }
+
+    function getGroupMembers(uint256 groupId) public view returns (address[] memory) {
+        return groups[groupId].members;
+    }
+
+    function _removeMember(uint256 groupId, address member) private {
+        require(isMemberOfGroup(groupId, member), "Member does not exist");
+        for (uint256 i = 0; i < groups[groupId].members.length; i++) {
+            if (groups[groupId].members[i] == member) {
+                groups[groupId].members[i] = groups[groupId].members[groups[groupId].members.length - 1];
+                groups[groupId].members.pop();
+                break;
+            }
+        }
     }
 }
